66fa230db8c3579b7e136ed52011503fdbe3edc4
SanaGitHub123 <122459112+SanaGitHubP@users.noreply.github.com>
2023-05-13 11:59:20 +0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBqTgZA1yZj-v3l070D9IgY4UhprtkAlGw",
+    authDomain: "projecttest-200e9.firebaseapp.com",
+    projectId: "projecttest-200e9",
+    storageBucket: "projecttest-200e9.appspot.com",
+    messagingSenderId: "923355677572",
+    appId: "1:923355677572:web:deb3b473cc69ffbd9f53d1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
